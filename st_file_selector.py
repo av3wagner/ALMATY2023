cbc72945376b8d4d7c93bc5cf120f79c8dd2c2e3
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-02-01 08:11:59 +0100

    python commit

---
+++ st_file_selector.py
@@ -0,0 +1,28 @@
+import os
+import streamlit as st
+
+
+def st_file_selector(st_placeholder, path='.', label='Please, select a file/folder...'):
+    # get base path (directory)
+    base_path = '.' if path is None or path is '' else path
+    base_path = base_path if os.path.isdir(
+        base_path) else os.path.dirname(base_path)
+    base_path = '.' if base_path is None or base_path is '' else base_path
+    # list files in base path directory
+    files = os.listdir(base_path)
+    if base_path is not '.':
+        files.insert(0, '..')
+    files.insert(0, '.')
+    selected_file = st_placeholder.selectbox(
+        label=label, options=files, key=base_path)
+    selected_path = os.path.normpath(os.path.join(base_path, selected_file))
+    if selected_file is '.':
+        return selected_path
+    if os.path.isdir(selected_path):
+        selected_path = st_file_selector(st_placeholder=st_placeholder,
+                                         path=selected_path, label=label)
+    return selected_path
+
+xpath = os.getcwd()
+st.markdown(xpath)
+st_file_selector(st_placeholder, path=xpath, label='Please, select a file/folder...')
