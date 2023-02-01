4cd6c1aa4788c87b69799d2a9abd6510bbcabbc5
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-02-01 08:28:57 +0100

    python commit

---
+++ Decode.py
@@ -0,0 +1,11 @@
+import base64
+import streamlit as st
+import zipfile
+import os
+import tempfile
+import shutil
+
+data = open("C:\ALMATY2023\IPYNB2023\AVaS2023FinishAbb.docx", "rb").read()
+encoded = base64.b64encode(data)
+decoded = base64.b64decode(encoded)
+st.download_button('Download Here', decoded, "C:\ALMATY2023\IPYNB2023\decoded_file.docx")
